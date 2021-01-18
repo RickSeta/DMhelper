c57d08ac6b927d11c080e5b11acd36e1c0a9248a
zesto <henrique.coutinho@zesto.com.br>
2021-01-18 00:43:25 -0300

    enhancement: better disposition of functions added

--- navigation/mainMenu.tsx
+++ navigation/mainMenu.tsx
@@ -2,18 +2,25 @@ import React from 'react'
 import styles from './styles'
 import AppButton from '../components/AppButton'
 import {Button, Text, TextInput, View} from 'react-native'
+import MenuInterface from './mainMenuMethods'
 
 const MainMenu = (props:any)=>{
-    const novoJogo = () => console.log('Novo jogo');
-    const retomaJogo = () => console.log('Retomei');
     return(
     <View>
         <Text style={styles.title}>
             Menu inicial
         </Text>
         
-        <AppButton onPress={()=>retomaJogo()} title="Retomar jogo" buttonStyle={styles.button} textStyle={styles.buttonText}></AppButton>
-        <AppButton onPress={()=>novoJogo()} title="Novo jogo" buttonStyle={styles.button} textStyle={styles.buttonText}></AppButton>
+        <AppButton 
+            onPress={()=> MenuInterface.restartGame()} 
+            title="Retomar jogo" 
+            buttonStyle={styles.button} 
+            textStyle={styles.buttonText}></AppButton>
+
+        <AppButton onPress={()=> MenuInterface.newGame()} 
+                    title="Novo jogo" 
+                    buttonStyle={styles.button}
+                    textStyle={styles.buttonText}></AppButton>
         
     </View>
     );
---
+++ navigation/mainMenuMethods.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class MenuInterface {
+    constructor(){
+
+    }
+    public static restartGame(){console.log('restart')}
+    public static newGame(){(console.log('new game'))};
+}
+export default MenuInterface;
